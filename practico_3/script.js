607e2707d8e4c6fb5038e2dc50f80e2b209942f6
ivan escobar <i.escobar@itecriocuarto.org.ar>
2025-04-04 01:17:35 -0300

    Mejoras de script.js del tp3

--- practico_3/script.js
+++ practico_3/script.js
@@ -1,14 +1,36 @@
-const peso = parseFloat(prompt("Ingrese su peso en kg(ej: 70.5)"));
-//parseFloat = sirve para transformar un string en decimales
-const altura = parseFloat(prompt("Ingrese su altura en metros(ej: 1.80)"));
+let peso; // fuera del bucle para que exista después
+let altura; // fuera del bucle
+ 
+// Bucle para pedir el peso 
+while (true) { 
+    const inputPeso = prompt("Ingrese su peso en kg (ej: 70.5)");
+    peso = parseFloat(inputPeso); // convertir la entrada a número
 
-if (isNaN(peso) || peso <= 0 || isNaN(altura) || altura <=0) {
-    alert("Por favor ingrese nuevamente");
-    // isNaN = is not a number comprueba si el dato es o puede transformarse en un dato numerico
-} else {  
-    let imc = peso / (altura * altura);
-    let imcRedondeado = imc.toFixed(1);
-    //toFixed
+    // si es un número (no NaN) Y si es mayor que 0
+    if (!isNaN(peso) && peso > 0) {
+        break; 
+        // Si es válido, salimos del bucle del peso
+    } else {
+        alert("Peso inválido. Ingrese Nuevamente.");
+        // Si no es válido, intentar de nuevo
+    }
+}
+// Bucle para pedir la altura solo despues del peso valido
+while (true) {
+    const inputAltura = prompt("Ingrese su altura en metros (ej: 1.80)");
+    altura = parseFloat(inputAltura); 
+    // convertir la entrada a número
+
+    if (!isNaN(altura) && altura > 0) {
+        break; 
+    } else {
+        alert("Altura inválida. Ingrese nuevamente.");
+        
+    }
+}
+let imc = peso / (altura * altura);
+let imcRedondeado = imc.toFixed(1);
+//toFixed para procesar numeros decimales
     let clase = "";
     if (imc < 18.5) {
         clase = "Tienes bajo peso.";
@@ -22,4 +44,4 @@ if (isNaN(peso) || peso <= 0 || isNaN(altura) || altura <=0) {
 
     let mensaje = `Tu IMC es ${imcRedondeado}. ${clase} `;
     alert(mensaje);
-}
+    
