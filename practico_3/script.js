c0df24e7d1134452accf5371e85b2df3f9f08c66
ivan escobar <i.escobar@itecriocuarto.org.ar>
2025-04-03 23:10:57 -0300

    correcciones y probando metodos para mejorar el tp

--- practico_3/script.js
+++ practico_3/script.js
@@ -1,17 +1,25 @@
-const peso = parseInt(prompt("Ingrese su peso en kg:"));
-const altura = parseInt(prompt("Ingrese su altura en metros:"));
+const peso = parseFloat(prompt("Ingrese su peso en kg(ej: 70.5)"));
+//parseFloat = sirve para transformar un string en decimales
+const altura = parseFloat(prompt("Ingrese su altura en metros(ej: 1.80)"));
 
-let imc = peso / (altura * altura);
-let mensaje = `Tu IMC es ${imc}. `;
+if (isNaN(peso) || peso <= 0 || isNaN(altura) || altura <=0) {
+    alert("Por favor ingrese nuevamente");
+    // isNaN = is not a number comprueba si el dato es o puede transformarse en un dato numerico
+} else {  
+    let imc = peso / (altura * altura);
+    let imcRedondeado = imc.toFixed(1);
+    //toFixed
+    let clase = "";
+    if (imc < 18.5) {
+        clase = "Tienes bajo peso.";
+    } else if (imc < 25) {
+        clase = "Tienes un peso normal.";
+    } else if (imc < 30) {
+        clase = "Tienes sobrepeso.";
+    } else {
+        clase = "Tienes obesidad.";
+    }
 
-if (imc < 18.5) {
-    mensaje += "Tienes bajo peso.";
-} else if (imc < 25) {
-    mensaje += "Tienes un peso normal.";
-} else if (imc < 30) {
-    mensaje += "Tienes sobrepeso.";
-} else {
-    mensaje += "Tienes obesidad.";
+    let mensaje = `Tu IMC es ${imcRedondeado}. ${clase} `;
+    alert(mensaje);
 }
-
-alert(mensaje);
