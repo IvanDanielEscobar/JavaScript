4d8ce86d1e3f9155efccd12ffe6c7a57db476619
ivan escobar <i.escobar@itecriocuarto.org.ar>
2025-04-10 20:50:13 -0300

    Agregos practico_interactivo2

---
+++ practico_interactivo2/script.js
@@ -0,0 +1,11 @@
+const usuario = "i.escobar";
+const contraseña = "1234.Ie";
+
+const usuarioStr = prompt("usuario:");
+const contraseñaStr = prompt("contraseña:");
+
+if (usuarioStr === usuario && contraseñaStr === contraseña) {
+    alert("bienvenido");
+} else {
+    alert("usuario o contraseña incorrecta");
+}
